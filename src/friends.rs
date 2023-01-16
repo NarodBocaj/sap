41e00a5a7bed8f13552bedf62a94e2cf81f50414
NarodBocaj <jmdoran19@yahoo.com>
2023-01-16 02:42:56 -0600

    added flamingo, cricket, fixes to ant

--- src/friends.rs
+++ src/friends.rs
@@ -1,20 +1,46 @@
-#[derive(Clone)]
+use rand::Rng;
+
+//#[derive(Clone)]
 pub struct Friend{
     pub attack: i32,
     pub health: i32,
     pub name: String,
-    pub tier: i8,
-    pub xp: i8,
+    pub tier: i32,
+    pub xp: i32,
 }
 
 
 impl Friend{
-    pub fn faint(&self, friendly_friends: &mut Vec<Friend>) -> Vec<Friend>{
+    pub fn faint(&self, friendly_friends: &mut Vec<Friend>, idx: i32) -> (){
+        let lvl = (self.xp / 3) + 1;
+        
         if self.name == "ant".to_string(){
-            println!("ant fainting");
-            friendly_friends[0].health += 1;
-            return friendly_friends.to_vec()
+            let rand_num = rand::thread_rng().gen_range(0..friendly_friends.len());
+            println!("ant fainting, rand inx ={}", rand_num);
+            friendly_friends[rand_num as usize].attack += 2 * lvl;
+            friendly_friends[rand_num as usize].health += 1 * lvl;
         }
-        return friendly_friends.to_vec()
+
+        if self.name == "cricket".to_string(){
+            let zombie_cricket = Friend{
+                attack: 1 * lvl,
+                health: 1 * lvl,
+                name: "zombie_cricket".to_string(),
+                tier: 1,
+                xp: 1,
+            };
+            friendly_friends.insert(idx as usize,zombie_cricket);
+        }
+
+        if self.name == "flamingo".to_string(){
+            if idx - 1 >= 0{
+                friendly_friends[(idx - 1) as usize].attack += 1 * lvl;
+                friendly_friends[(idx - 1) as usize].health += 1 * lvl;
+            }
+            if idx - 2 >= 0{
+                friendly_friends[(idx - 2) as usize].attack += 1 * lvl;
+                friendly_friends[(idx - 2) as usize].health += 1 * lvl;
+            }
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -8,23 +8,45 @@ fn main() {
         health: 1,
         name: "ant".to_string(),
         tier: 1,
-        xp: 0,
+        xp: 1,
     };
     let ant1 = friends::Friend{
         attack: 2,
         health: 1,
         name: "ant".to_string(),
         tier: 1,
-        xp: 0,
+        xp: 3,
+    };
+    let flamingo = friends::Friend{
+        attack: 4,
+        health: 2,
+        name: "flamingo".to_string(),
+        tier: 2,
+        xp: 6,
+    };
+    let cricket = friends::Friend{
+        attack: 1,
+        health: 2,
+        name: "cricket".to_string(),
+        tier: 1,
+        xp: 1,
     };
 
     friendly_friends.push(ant);
     friendly_friends.push(ant1);
+    friendly_friends.push(flamingo);
+    friendly_friends.push(cricket);
 
-    let dead_pet = friendly_friends.pop();
+    let dead_pet: friends::Friend = (friendly_friends.pop()).unwrap();
 
-    friendly_friends = (dead_pet.faint(&mut friendly_friends)).to_vec();
+    dead_pet.faint(&mut friendly_friends, 3);
 
     
-    println!("Ant's health is: {}", friendly_friends[0].health);
+    print_friends(&friendly_friends);
+}
+
+fn print_friends(friendly_friends: & Vec<friends::Friend>) -> (){
+    for i in 0..friendly_friends.len(){
+        println!("{} {}/{}", friendly_friends[i].name, friendly_friends[i].attack, friendly_friends[i].health);
+    }
 }
